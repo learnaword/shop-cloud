29df1217bcf9f61295397b02b6696e9561d58564
majunliang <875657453@qq.com>
2023-07-30 17:44:16 +0800

    初始：搭建微服务项目整体架构

---
+++ shop-service-pay/shop-service-pay-biz/src/main/java/com/shop/service/pay/shop/PayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shop.service.pay.shop;
+
+/**
+ * Hello world!
+ *
+ */
+public class PayServiceApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ shop-service-pay/shop-service-pay-biz/src/main/java/com/shop/service/pay/shop/dal/order.java
@@ -0,0 +1,8 @@
+package com.shop.service.pay.shop.dal;
+
+
+
+public class order {
+
+    private int id;
+}
